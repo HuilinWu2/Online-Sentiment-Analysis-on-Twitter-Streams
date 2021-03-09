0a8a99f04a8994209e1e29182f69b3362b1ae5b0
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2021-03-09 12:41:43 +0100

    Update stream_based_tweets_textpreprocess_vectorization.py

--- Pyflink_demo/Streaming_demo_Sentiment_Analysis/stream_based_tweets_textpreprocess_vectorization.py
+++ Pyflink_demo/Streaming_demo_Sentiment_Analysis/stream_based_tweets_textpreprocess_vectorization.py
@@ -9,42 +9,36 @@ for twts in all_tweets:
     tweets.append(twts.replace('\n',''))
 label = [0]*200+[4]*200
 for i in range(len(tweets)):
-    twitter_stream.append((tweets[i],label[i]))
-
-# def load_model():
-#     import redis
-#     import pickle
-#     import logging
-#     from sklearn.naive_bayes import BernoulliNB
-
-#     r = redis.StrictRedis(host='localhost', port=6379, db=0)
-#     try:
-#         called_model = pickle.loads(r.get('osamodel'))
-#     except TypeError:
-#         logging.info('The model name you entered cannot be found in redis, now we initialize a model')
-#     except (redis.exceptions.RedisError, TypeError, Exception):
-#         logging.warning('There is a problem with Redis, now we initialize a model')
-#     finally:
-#         called_model = called_model or BernoulliNB
-
-#     return called_model
-
-
+    twitter_stream.append((tweets[i],label[i]))   
+    
+'''
+Data form:
+tuple:(tweet,label)
+'''
+    
+# necessary import for Flink environment
+from pyflink.common.serialization import SimpleStringEncoder
+from pyflink.common.typeinfo import Types
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors import StreamingFileSink    
+    
+# vectorization function
 def vectorization(text,label):
     from nltk.corpus import stopwords
     from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, CountVectorizer
+    
+    # TF-IDF
     countvector    = CountVectorizer(stop_words= stopwords.words('english')) 
     tfidftransform = TfidfTransformer()
     X = countvector.fit_transform([text]).toarray() 
     X_train = tfidftransform.fit_transform(X).toarray()
+    
+    # return tuple of vectorized train_data and label
     return (X_train,label)
 
-from pyflink.common.serialization import SimpleStringEncoder
-from pyflink.common.typeinfo import Types
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors import StreamingFileSink
-
+# text preprocessing/cleaning
 def textprocess(text,label):
+    # using regular formulation
     import re
     text = re.sub(r'\W', ' ', text)
     text = re.sub(r'^br$', ' ', text)
@@ -55,11 +49,16 @@ def textprocess(text,label):
     text = text.lower().strip()
     vectorization(text,label)
 
+# create streaming enviroment of Flink
 env = StreamExecutionEnvironment.get_execution_environment()
 env.set_parallelism(1)
+
+# data from Twitter Stream
 ds = env.from_collection(
     collection=twitter_stream,
     type_info=Types.ROW([Types.STRING(), Types.INT()]))
 ds.map(lambda x: textprocess(x[0],x[1]))\
   .print()
-env.execute("tutorial_job")
+
+# exectue job
+env.execute("demo_clean_vectorization_job")
