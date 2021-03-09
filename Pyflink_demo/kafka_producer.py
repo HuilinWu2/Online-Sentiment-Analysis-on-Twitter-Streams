db24a2f50b5c09d523ffc26898e552848a6b80f3
HuilinWu2 <68987242+HuilinWu2@users.noreply.github.com>
2021-03-09 23:27:57 +0100

    Add files via upload

---
+++ Pyflink_demo/kafka_producer.py
@@ -0,0 +1,47 @@
+import numpy as np
+from json import dumps
+from time import sleep
+from sklearn import datasets
+from datetime import datetime
+from kafka import KafkaProducer
+
+#=====================================================
+max_msg_per_second = 10  
+topic = "stream_tweets"  # kafka topic
+bootstrap_servers = ['localhost:9092']
+
+
+def write_data():
+    twitter_stream= []
+    tweets = []
+    label = []
+    all_tweets = open("twitter_140.txt")  
+    for twts in all_tweets:  
+        tweets.append(twts.replace('\n',''))
+    label = [0]*200+[4]*200
+
+
+    all_x = tweets
+    all_y = label
+
+    # producer initialized
+    producer = KafkaProducer(
+        bootstrap_servers=bootstrap_servers,
+        value_serializer=lambda x: dumps(x).encode('utf-8')
+    )
+
+    while True:
+
+        for x, y in zip(all_x, all_y):
+            # produce data and send to kafka
+            cur_data = {
+                "ts": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "x": x.tolist(),
+                "actual_y": int(y)
+            }
+            producer.send(topic, value=cur_data)
+
+            sleep(1 / max_msg_per_second)
+
+
+write_data()
